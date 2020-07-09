4377a9a4f83a84491841d5aedecbd585dfb53f83
Sri Hari Krishna Narayanan <sriharikrishna@gmail.com>
2020-07-09 14:14:18 -0500

    Fix scripts

--- src/script_bfgs_pnm.sh
+++ src/script_bfgs_pnm.sh
@@ -1,2 +1,11 @@
 make -f MakefileTapf clean; make -f Makefile clean; make -f Makefile CASE=pnm prep ; make -f MakefileTapf ALL=1 BFGS=1 CASE=pnm
-time ./nmadv < pnm/av18uix_pnm.in > out_tap_all_bfgs_pnm
+cat pnm/av18uix_pnm.in circle.txt >temp.dat
+echo $1 >> temp.dat
+echo $2 >> temp.dat
+
+start=`date +%s`
+time ./nmadv < temp.dat > out_tap_all_bfgs_pnm_$1_$2
+end=`date +%s`
+runtime=$((end-start))
+
+echo runtime $runtime >> out_tap_all_bfgs_pnm_$1_$2
--- src/script_dfo_snm.sh
+++ src/script_dfo_snm.sh
@@ -1,8 +1,11 @@
-
-#make -f MakefileTapf clean; make -f Makefile clean; make -f Makefile CASE=snm prep ; make -f MakefileTapf ALL=1 CASE=snm numCore_dv.pre.f
-#exit
 make -f MakefileTapf clean; make -f Makefile clean; make -f Makefile CASE=snm prep ; make -f MakefileTapf ALL=1 CASE=snm
 cat snm/av18uix.in circle.txt >temp.dat
 echo $1 >> temp.dat 
 echo $2 >> temp.dat 
-./nmadv < temp.dat > out_tap_all_dfo
+
+start=`date +%s`
+time ./nmadv < temp.dat > out_tap_all_dfo_snm_$1_$2
+end=`date +%s`
+runtime=$((end-start))
+
+echo runtime $runtime >> out_tap_all_dfo_snm_$1_$2
