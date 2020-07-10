b0d52ef85540505cb4efe961aa3b216e3ca19520
Sri Hari Krishna Narayanan <sriharikrishna@gmail.com>
2020-07-10 10:23:09 -0400

    Support FULLX

---
+++ src/script_bfgs_snm_fullx.sh
@@ -0,0 +1,11 @@
+make -f MakefileTapf clean; make -f Makefile clean; make -f Makefile CASE=snm prep ; make -f MakefileTapf ALL=1 BFGS=1 FULLX=1 CASE=snm
+cat snm/av18uix.in circle.txt >temp.dat
+echo $1 >> temp.dat
+echo $2 >> temp.dat
+
+start=`date +%s`
+time ./nmadv < temp.dat > out_tap_all_bfgs_snm_$1_$2
+end=`date +%s`
+runtime=$((end-start))
+
+echo runtime $runtime >> out_tap_all_bfgs_snm_$1_$2
