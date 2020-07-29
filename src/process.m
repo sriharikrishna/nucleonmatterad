7471726230e3722a0ae3ae1a4597bfdacd055d74
Sri Hari Krishna Narayanan <sriharikrishna@gmail.com>
2020-07-29 13:43:04 -0400

    Postprocessing script

--- src/process.m
+++ src/process.m
@@ -3,13 +3,14 @@ clear all
 function someval = read_plot(n,filename,yscale)
   dim=2+2*n;
   #filename='dfo_snm'
-  for j=1:60
+  j=0;
+  for filno=1:30
     for d=1:2
-      perturbstr=int2str(j);
-      if j<10
+      perturbstr=int2str(filno);
+      if filno<10
         perturbstr=sprintf('%s%s',"0",perturbstr);
       end
-      filenamelocal=sprintf('%s%s%s%d%s','dfo_snm_', perturbstr,'_',d);
+      filenamelocal=sprintf('%s%s%s%s%d%s',filename, '_',perturbstr,'_',d);
       % Next section is a per-nucleus summary of the different Fayans computations
       % that were run to collect all the required theoretical results.
       fid = fopen([filenamelocal,'.txt']);
@@ -38,24 +39,27 @@ function someval = read_plot(n,filename,yscale)
     prob=0;
     datastore(datastore==0)=NaN;
     figure(1); clf; hold on;
+    #Loop over all available problems(up to 60)
     for j=1:size(probstore,1)
       if probstore(j,2)==step
         prob=prob+1;
-        figure(1)
-        plot(datastore(:,2,j))
+        funval(:,prob)=datastore(:,2,j);
         best(1,prob) = datastore(1,2,j);
         grad(1,prob) = norm(datastore(1,n+3:dim,j));
+        #Loop over all optimizer iterations in the problem
         for k=2:length(datastore(:,2,j))
             best(k,prob) = min(best(k-1,prob),datastore(k,2,j));
             grad(k,prob) = norm(datastore(k,n+3:dim,j));
         end
       end
     end
-
-    figure(1);
+    figure(1)
+    # Plot the function values from iteration 3 instead of 1
+    # because the values of iteration 2 is very high in some
+    # cases and leads to lower values not being plotted
+    plot(funval(3:end,:))
     box on
     axis tight
-    set(gca,'yscale',yscale)
     xlabel('iterations')
     ylabel('f value')
     title(sprintf('%s %f',filename, step))
@@ -70,7 +74,7 @@ function someval = read_plot(n,filename,yscale)
     plot(best)
     box on
     axis tight
-    set(gca,'yscale',yscale)
+    set(gca,'yscale','linear')
     xlabel('iterations')
     ylabel('best f value')
     title(sprintf('%s %f',filename, step))
@@ -85,7 +89,7 @@ function someval = read_plot(n,filename,yscale)
     plot(grad)
     box on
     axis tight
-    set(gca,'yscale',yscale)
+    set(gca,'yscale','log')
     xlabel('iterations')
     ylabel('gradient norm')
     title(sprintf('%s %f',filename, step))
@@ -98,7 +102,7 @@ function someval = read_plot(n,filename,yscale)
   end
   some_val = 0; % could be scalar, vector, matrix etc.
 end
-read_plot(2,'dfo_snm','linear')
 read_plot(2,'bfg_snm','linear')
+read_plot(2,'bfg_pnm','linear')
+read_plot(4,'dfo_snm','linear')
 read_plot(4,'dfo_pnm','linear')
-read_plot(4,'bfg_pnm','linear')
