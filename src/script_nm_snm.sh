7ac37c96c6d20d7a0b3ee8a46877c9ba041e0dec
Sri Hari Krishna Narayanan <sriharikrishna@gmail.com>
2020-11-06 17:38:42 -0600

    NUCMAT scripts bugfix

--- src/script_nm_snm.sh
+++ src/script_nm_snm.sh
@@ -1,10 +1,9 @@
-#make -f MakefileTapf clean; make -f Makefile clean; make -f Makefile CASE=snm prep ; make -f MakefileTapf ALL=1 NUCMAT=1 CASE=snm
-cat snm/av18uix.in  >temp_$1_$2.dat
-echo $1 >> temp_$1_$2.dat
-echo $2 >> temp_$1_$2.dat
+make -f MakefileTapf clean; make -f Makefile clean; make -f Makefile CASE=snm prep ; make -f MakefileTapf ALL=1 NUCMAT=1 CASE=snm
+cat snm/av18uix.in >temp_$1_$2.dat
+echo $1 $2 >> temp_$1_$2.dat
 start=`date +%s`
-time ./nmadv < temp_$1_$2.dat > out_tap_all_nm_snm_$1_$2
+time ./nmadv < temp_$1_$2.dat > out_tap_all_nucmat_snm_$1_$2
 end=`date +%s`
 runtime=$((end-start))
 
-echo runtime $runtime >> out_tap_all_nm_snm_$1_$2
+echo runtime $runtime >> out_tap_all_nucmat_snm_$1_$2
--- src/script_nm_snm_fullx.sh
+++ src/script_nm_snm_fullx.sh
@@ -1,15 +1,9 @@
-#make -f MakefileTapf clean; make -f Makefile clean; make -f Makefile CASE=snm prep ; make -f MakefileTapf ALL=1 NUCMAT=1 FULLX=1 CASE=snm
-cat snm/av18uix.in  >temp_$1_$2.dat
-echo $1 >> temp_$1_$2.dat
-echo $2 >> temp_$1_$2.dat
-echo $3 >> temp_$1_$2.dat
-echo $4 >> temp_$1_$2.dat
-echo $5 >> temp_$1_$2.dat
-echo $6 >> temp_$1_$2.dat
-echo $7 >> temp_$1_$2.dat
+make -f MakefileTapf clean; make -f Makefile clean; make -f Makefile CASE=snm prep ; make -f MakefileTapf ALL=1 FULLX=1 NUCMAT=1 CASE=snm
+cat snm/av18uix.in >temp_$1_$2_$3_$4_$5_$6_$7.dat
+echo $1 $2 $3 $4 $5 $6 $7 >> temp_$1_$2_$3_$4_$5_$6_$7.dat
 start=`date +%s`
-time ./nmadv < temp_$1_$2.dat > out_tap_all_nm_snm_$1_$2
+time ./nmadv < temp_$1_$2_$3_$4_$5_$6_$7.dat > out_tap_all_nucmat_snm_$1_$2_$3_$4_$5_$6_$7
 end=`date +%s`
 runtime=$((end-start))
 
-echo runtime $runtime >> out_tap_all_nm_snm_$1_$2
+echo runtime $runtime >> out_tap_all_nucmat_snm_$1_$2_$3_$4_$5_$6_$7
