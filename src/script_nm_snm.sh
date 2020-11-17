58acaac9d56fe239071a85d04d7357144e2438ed
Sri Hari Krishna Narayanan <sriharikrishna@gmail.com>
2020-11-17 14:04:48 -0600

    Run plain code for only one iteration with custom inputs

--- src/script_nm_snm.sh
+++ src/script_nm_snm.sh
@@ -1,9 +1,10 @@
-make -f MakefileTapf clean; make -f Makefile clean; make -f Makefile CASE=snm prep ; make -f MakefileTapf ALL=1 NUCMAT=1 CASE=snm
-cat snm/av18uix.in >temp_$1_$2.dat
-echo $1 $2 >> temp_$1_$2.dat
+mkdir -p snm/obj/; make clean; make -f MakefileTapf clean; make prep CASE=snm; make CASE=snm CUSTOM_INPUTS=1 NUCMAT=1
+cat snm/av18uix.in >temp.dat
+echo $1 $2 >> temp.dat 
+
 start=`date +%s`
-time ./nmadv < temp_$1_$2.dat > out_tap_all_nucmat_snm_$1_$2
+time ./snm/snm.x < temp.dat > out_nm_snm_$1_$2
 end=`date +%s`
 runtime=$((end-start))
 
-echo runtime $runtime >> out_tap_all_nucmat_snm_$1_$2
+echo runtime $runtime >> out_nm_snm_$1_$2
