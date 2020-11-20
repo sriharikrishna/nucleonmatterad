55d106c753d2ef80d0d15753969695191e94e005
Sri Hari Krishna Narayanan <sriharikrishna@gmail.com>
2020-11-20 10:35:16 -0600

    Change the name of temporary files

--- src/script_dfo_pnm.sh
+++ src/script_dfo_pnm.sh
@@ -1,9 +1,9 @@
 #make -f MakefileTapf clean; make -f Makefile clean; make -f Makefile CASE=pnm prep ; make -f MakefileTapf ALL=1 CASE=pnm CUSTOM_INPUTS=1
-cat pnm/av18uix_pnm.in >temp.dat
-echo $1 $2 $3 $4 >> temp.dat 
+cat pnm/av18uix_pnm.in >temp_$1_$2_$3_$4.dat
+echo $1 $2 $3 $4 >> temp_$1_$2_$3_$4.dat 
 
 start=`date +%s`
-time ./nmadv < temp.dat > out_tap_all_dfo_pnm_$1_$2_$3_$4
+time ./nmadv < temp_$1_$2_$3_$4.dat > out_tap_all_dfo_pnm_$1_$2_$3_$4
 end=`date +%s`
 runtime=$((end-start))
 
--- src/script_dfo_snm.sh
+++ src/script_dfo_snm.sh
@@ -1,9 +1,9 @@
 #make -f MakefileTapf clean; make -f Makefile clean; make -f Makefile CASE=snm prep ; make -f MakefileTapf ALL=1 CASE=snm CUSTOM_INPUTS=1
-cat snm/av18uix.in >temp.dat
-echo $1 $2 >> temp.dat 
+cat snm/av18uix.in >temp_$1_$2.dat
+echo $1 $2 >> temp_$1_$2.dat 
 
 start=`date +%s`
-time ./nmadv < temp.dat > out_tap_all_dfo_snm_$1_$2
+time ./nmadv < temp_$1_$2.dat > out_tap_all_dfo_snm_$1_$2
 end=`date +%s`
 runtime=$((end-start))
 
