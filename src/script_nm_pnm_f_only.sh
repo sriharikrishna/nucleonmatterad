93b52697470792c4a64eee552281112fa80e9862
Sri Hari Krishna Narayanan <sriharikrishna@gmail.com>
2021-04-20 17:33:01 -0500

    Affix the input to the name of the temporary file

--- src/script_nm_pnm_f_only.sh
+++ src/script_nm_pnm_f_only.sh
@@ -1,9 +1,9 @@
 #mkdir -p pnm/obj/; make clean; make -f MakefileTapf clean; make prep CASE=pnm; make CASE=pnm CUSTOM_INPUTS=1 NUCMAT=1
-cat pnm/av18uix_pnm.in >temp.dat
-echo $1 $2 $3 $4 >> temp.dat 
+cat pnm/av18uix_pnm.in >temp_$1_$2_$3_$4.dat
+echo $1 $2 $3 $4 >> temp_$1_$2_$3_$4.dat 
 
 start=`date +%s`
-time ./pnm/pnm.x < temp.dat > out_nm_pnm_$1_$2_$3_$4
+time ./pnm/pnm.x < temp_$1_$2_$3_$4.dat > out_nm_pnm_$1_$2_$3_$4
 end=`date +%s`
 runtime=$((end-start))
 
