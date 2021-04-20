a30767e957b8122cf36edeabfd4ae828e033af56
Sri Hari Krishna Narayanan <sriharikrishna@gmail.com>
2021-04-20 12:05:02 -0500

    Modify the preprocessor directive

--- src/nm.f
+++ src/nm.f
@@ -153,7 +153,7 @@ c
       if (n.eq.4) then
         bst=x(3)
         btn=x(4)
-#if CASE==snm
+#if defined (CASE_SNM)
         bls=bst
 #else
         bls=0.
@@ -276,11 +276,11 @@ c     l3=2*l3
       if (n.eq.4) then
         bst=x(3)
         btn=x(4)
-#if CASE==snm
+!#if defined (CASE_SNM)
         bls=bst
-#else
-        bls=0.
-#endif
+!#else
+!        bls=0.
+!#endif
       end if
       call nmmain(np,nv,nt,ni,nie,no,ns,lf,lc,ls,lt,ll,lg,le,l3,lk
      &           ,dor,bst,btn,bls,npi,npf, gint, endiff, efree)
