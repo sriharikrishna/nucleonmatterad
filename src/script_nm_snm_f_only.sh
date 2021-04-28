dca1a189801d2301fc9854d94ff37e2894e6188a
Matt Menickelly <mmenickelly@anl.gov>
2021-04-28 13:22:25 -0500

    Making temp files print correctly in SNM case

--- src/script_nm_snm_f_only.sh
+++ src/script_nm_snm_f_only.sh
@@ -1,9 +1,9 @@
-mkdir -p snm/obj/; make clean; make -f MakefileTapf clean; make prep CASE=snm; make CASE=snm CUSTOM_INPUTS=1 NUCMAT=1
-cat snm/av18uix.in >temp.dat
-echo $1 $2 >> temp.dat 
+#mkdir -p snm/obj/; make clean; make -f MakefileTapf clean; make prep CASE=snm; make CASE=snm CUSTOM_INPUTS=1 NUCMAT=1
+cat snm/av18uix.in >temp_$1_$2.dat
+echo $1 $2 >> temp_$1_$2.dat 
 
 start=`date +%s`
-time ./snm/snm.x < temp.dat > out_nm_snm_$1_$2
+time ./snm/snm.x < temp_$1_$2.dat > out_nm_snm_$1_$2
 end=`date +%s`
 runtime=$((end-start))
 
